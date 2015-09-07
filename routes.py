f114181594f84ec56e2140c3b88c26d97c1260dd
Lalith Polepeddi <lpolepeddi@gmail.com>
2015-09-07 15:09:36 -0400

    Add gzip

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_sslify import SSLify
+from flask.ext.compress import Compress
 
 app = Flask(__name__)
-sslify = SSLify(app, subdomains=True)
+sslify = SSLify(app)
+Compress(app)
 
 @app.route("/")
 def hello():
