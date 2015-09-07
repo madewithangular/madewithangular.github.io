dfd4e6451db02a769b8a7db09b635e6bf26ade54
Lalith Polepeddi <lpolepeddi@gmail.com>
2015-09-07 16:11:01 -0400

    Add Cache Control to /

--- routes.py
+++ routes.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from flask_sslify import SSLify
 from flask.ext.compress import Compress
 
 app = Flask(__name__)
-sslify = SSLify(app)
+# sslify = SSLify(app)
 Compress(app)
 
 @app.route("/")
-def hello():
-  return render_template('index.html')
+def index():
+  response = make_response(render_template('index.html'))
+  response.headers['cache-control'] = 'must_revalidate, public, max-age=3600'
+  return response
 
 if __name__ == "__main__":
   app.run()
